3827ee71022361e3cd10b33702c429d314504239
GuiBrum22 <guilhermegamabrum@gmail.com>
2024-02-16 14:33:28 -0300

    ok

--- calculadora_flutter/lib/main.dart
+++ calculadora_flutter/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
-
 void main() {
   runApp(CalculadoraApp());
 }
-
 
 class CalculadoraApp extends StatelessWidget {
   @override
@@ -16,37 +14,36 @@ class CalculadoraApp extends StatelessWidget {
   }
 }
 
-
 class Calculadora extends StatefulWidget {
   @override
   _CalculadoraState createState() => _CalculadoraState();
 }
-
 
 class _CalculadoraState extends State<Calculadora> {
   TextEditingController _controllerNumero1 = TextEditingController();
   TextEditingController _controllerNumero2 = TextEditingController();
   String _resultado = '';
 
-
   void _calcular(String operacao) {
     double numero1 = double.tryParse(_controllerNumero1.text) ?? 0.0;
     double numero2 = double.tryParse(_controllerNumero2.text) ?? 0.0;
-    double resultado;
-
+    double? resultado;
 
     setState(() {
       if (operacao == "Somar") {
         resultado = numero1 + numero2;
       } else if (operacao == 'Sub') {
         resultado = numero1 - numero2;
-      } else {
+      } else if (operacao == 'Mult') {
         resultado = numero1 * numero2;
+      } else if (operacao == 'Div'){
+        resultado = numero2 != 0 ? numero1 / numero2 : null;
+      } else {
+        resultado = numero2 != 0 ? numero1 / numero2 : null;
       }
-      _resultado = 'O Resultado é $resultado';
+      _resultado = resultado != null ? 'Resultado: $resultado' : 'Erro na operação';
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -86,6 +83,16 @@ class _CalculadoraState extends State<Calculadora> {
               onPressed: () => _calcular('Mult'),
               child: Text('Multiplicar'),
             ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () => _calcular('Div'),
+              child: Text('Dividir'),
+            ),
+            SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () => _calcular('Pot'),
+              child: Text('Potência'),
+            ),
             SizedBox(height: 16.0),
             Text(_resultado,
                 style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
@@ -95,7 +102,3 @@ class _CalculadoraState extends State<Calculadora> {
     );
   }
 }
-
-
-
-
