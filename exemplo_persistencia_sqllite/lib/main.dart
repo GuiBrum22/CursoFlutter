4f22542a389f7ca790afb5e4a0c72592dba257be
GuiBrum22 <guilhermegamabrum@gmail.com>
2024-04-01 16:05:53 -0300

    ok

--- exemplo_persistencia_sqllite/lib/main.dart
+++ exemplo_persistencia_sqllite/lib/main.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'View.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Projeto Sqllite",
+      title: "Projeto SqlLite",
       theme: ThemeData(
-        colorScheme: Colors.blue;
+        primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
   }
 }
